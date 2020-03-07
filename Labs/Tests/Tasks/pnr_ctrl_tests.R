1f74b4ca2f2587a0afda362c2c6ba1e5d4f4aab6
josefwilzen <josefwilzen@gmail.com>
2020-03-07 12:56:01 +0100

    updates

--- Labs/Tests/Tasks/pnr_ctrl_tests.R
+++ Labs/Tests/Tasks/pnr_ctrl_tests.R
@@ -2,25 +2,32 @@
 context("pnr_ctrl()")
 
 test_that("Kontroll av pnr_ctrl.", {
-  expect_that(exists("pnr_ctrl"), is_true(),
+  expect_true(exists("pnr_ctrl"),
               info = "Fel: pnr_ctrl() saknas.")
   expect_that(pnr_ctrl, is_a("function"),
               info = "Fel: pnr_ctrl är inte en funktion.")
   expect_function_self_contained(object = pnr_ctrl,
                         "Fel: Funktionen innehåller fria variabler")
-  expect_that(all(names(formals(pnr_ctrl)) %in% c("pnr")), condition=is_true(),
+  
+  expect_true(all(names(formals(pnr_ctrl)) %in% c("pnr")),
               info = "Fel: Argumenten i funktionen har felaktiga namn.")
-  expect_that(is.logical(pnr_ctrl("196408233234")), condition=is_true(),
+  
+  expect_true(is.logical(pnr_ctrl("196408233234")),
               info = "Fel: Funktionen returnerar inte ett logiskt element.")
-  expect_that(pnr_ctrl("196408233234"), condition=is_true(),
+  
+  expect_true(pnr_ctrl("196408233234"),
               info = "Fel: Ska returnera TRUE för 196408233234.")
-  expect_that(pnr_ctrl("196408233238"), condition=is_false(),
+  
+  expect_false(pnr_ctrl("196408233238"),
               info = "Fel: Ska returnera FALSE för 196408233238.")
-  expect_that(pnr_ctrl("198112189876"), condition=is_true(),
+  
+  expect_true(pnr_ctrl("198112189876"),
               info = "Fel: Ska returnera TRUE för 198112189876.")
-  expect_that(pnr_ctrl("198112189879"), condition=is_false(),
+  
+  expect_false(pnr_ctrl("198112189879"),
               info = "Fel: Ska returnera FALSE för 198112189879.")
-  expect_that(pnr_ctrl("199909090909"), condition=is_false(),
+  
+  expect_false(pnr_ctrl("199909090909"),
               info = "Fel: Ska returnera FALSE för 199909090909.")
   
   expect_function_code(object = pnr_ctrl, expected = "return", 
