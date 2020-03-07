60fc20bdd6e3db5e671efc00b98f1456a111e648
josefwilzen <josefwilzen@gmail.com>
2020-03-07 13:57:44 +0100

    updates

--- Labs/Tests/Tasks/pnr_sex_tests.R
+++ Labs/Tests/Tasks/pnr_sex_tests.R
@@ -2,16 +2,21 @@
 context("pnr_sex()")
 
 test_that("Kontroll av pnr_sex.", {
-  expect_that(exists("pnr_sex"), is_true(),
+  expect_true(exists("pnr_sex"),
               info = "Fel: pnr_sex() saknas.")
+  
   expect_that(pnr_sex, is_a("function"),
               info = "Fel: pnr_sex är inte en funktion.")
+  
   expect_function_self_contained(object = pnr_sex,
                         "Fel: Funktionen innehåller fria variabler")
-  expect_that(all(names(formals(pnr_sex)) %in% c("pnr")), condition=is_true(),
+  
+  expect_true(all(names(formals(pnr_sex)) %in% c("pnr")),
               info = "Fel: Argumenten i funktionen har felaktiga namn.")
-  expect_that(is.character(pnr_sex("196408233234")), condition=is_true(),
+  
+  expect_true(is.character(pnr_sex("196408233234")),
               info = "Fel: Funktionen returnerar inte ett textelement.")
+  
   expect_equal(pnr_sex("196408233234"), "M",
               info = "Fel: Ska returnera 'M' för 196408233234.")
   expect_equal(pnr_sex("196408233224"), "K",
